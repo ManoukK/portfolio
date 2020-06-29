5bf95b39da68adf8ccb02403bf559f765da8aa0e
ManoukK <manouk.kappe21@gmail.com>
2020-06-29 10:40:04 +0200

    offline pagina

---
+++ public/service-worker.js
@@ -0,0 +1,64 @@
+cacheName = 'v3'
+urlsToCache = [
+    '/css/index.css',
+    '/offline',
+    '/images/papertexture2.png'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('activating!!!');
+    event.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+      .then(function(){
+          self.skipWaiting();
+      })
+    );
+  });
+
+self.addEventListener('activate', function (event) {
+    console.log('activating!');
+    //verwijder alle cache namen die niet overeen komen met urlsToCache
+    event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.keys().then(requests => {
+					return Promise.all(
+						requests.filter(request => {
+							return !urlsToCache.includes(getPathName(request.url));
+						}).map(cacheName => {
+							return cache.delete(cacheName)
+						})
+					)
+				}
+			).then(() => self.clients.claim())
+		})
+	)
+});
+
+self.addEventListener('fetch', function(event){
+        event.respondWith(
+            caches
+                .match(event.request)
+                .then(function(response) {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                    })
+                .catch(function() {
+                    return caches.match('/offline');
+                })
+        );
+});
+
+/**
+ * Get a pathname from a full URL by stripping off domain
+ *
+ * @param {Object} requestUrl        The request object, e.g. https://www.mydomain.com/index.css
+ * @returns {String}                Relative url to the domain, e.g. index.css
+ */
+function getPathName(requestUrl) {
+	const url = new URL(requestUrl);
+	return url.pathname;
+}
--- server.js
+++ server.js
@@ -2,22 +2,32 @@ const express = require('express')
 const app = express()
 const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
+const revManifest = require('./static/rev-manifest');
+
+app.use(/.*-[0-9a-f]{10}\..*/, function(req, res, next){
+    res.header('Cache-Control', 'max-age=365000000');
+    next();
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('static'));
 
 app.get('/', function(req, res){
-    res.render('main')
+    res.render('main', { revManifest })
 })
 
 app.get('/about', function(req, res){
-    res.render('about')
+    res.render('about', { revManifest })
 })
 
 app.get('/cv', function(req, res){
-    res.render('cv')
+    res.render('cv', { revManifest })
 })
+
+app.get('/offline', function(req, res){
+    res.render('offline', { revManifest })
+});
 
 
 app.listen(port, host, function() {
